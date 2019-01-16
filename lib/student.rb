e007de400f650dcf47d134bac8df2cb0b96057e7
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-16 16:50:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,24 +3,25 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-def initialize(name, grade, id = nil)
-  @name = name
-  @grade = grade
-  @id = id
-end
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
-def.create_table
-  sql = <<-SQL 
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-    );
-    SQL 
+  def.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+    SQL
 
     DB[:conn].execute(sql)
-    end
-    
+  end
+
+  
 
 
-end 
+end
