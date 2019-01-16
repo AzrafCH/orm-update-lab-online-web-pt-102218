fa2c957983dd8223a01dc0124e706987c7a40366
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-16 17:08:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,9 @@ attr_reader :id
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
